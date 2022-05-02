90775133d36f529343a943f8f6427f4de6a5d51c
AdamzkT <91147017+AdamzkT@users.noreply.github.com>
2022-05-02 17:46:21 +0200

    Add files via upload

---
+++ source/drill27/drill27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void f(char* p, int x){
+    printf("p is \"%s\" and x is %d.\n", p, x);
+}
+
+int main() {
+    printf("Hello world!\n");
+    
+    char s_1[] = "Hello";
+	char s_2[] = "World!";
+	printf("%s %s\n", s_1, s_2);
+	
+	f("foo",7);
+	f("alma",12);
+	f("abc",1);
+    
+    return 0;
+}
