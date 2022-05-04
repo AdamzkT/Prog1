1f174e327008c5639402596b89fab92a9b5acf41
AdamzkT <91147017+AdamzkT@users.noreply.github.com>
2022-05-04 10:14:23 +0200

    Update drill16.cpp

--- source/drill16/drill16.cpp
+++ source/drill16/drill16.cpp
@@ -1,5 +1,5 @@
 /*
-    g++ drill16.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill16 `fltk-config --ldflags --use-images` -std=c++11
+    g++ drill16.cpp Graph.cpp Window.cpp GUI.cpp Lines_window.cpp -o drill16 `fltk-config --ldflags --use-images` -std=c++11
 */
 #include "Lines_window.h"
 #include "Graph.h"
